8e08237b971d8a8400457d2984fb5966e8a5d264
hodophile <71829615+hodophile@users.noreply.github.com>
2021-05-19 01:05:34 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -5,6 +5,7 @@ int main(void){
 	int type = get_int("Enter the type of pattern (1 or 2) ");
 	int rows = get_int("Number of rows ");
 	if(type == 1){
+		printf("\n");
 		for(int i=rows;i>0;i--){
 			for(int j=0;j<i;j++){
 				printf("#");
